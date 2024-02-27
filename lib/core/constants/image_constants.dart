97e7f31848ff583f6c2fbcd151d472721a9df676
Ajaspj <ajaspj123@gmail.com>
2024-02-27 22:07:06 +0530

    27.02

---
+++ lib/core/constants/image_constants.dart
@@ -0,0 +1,6 @@
+class ImageConstants {
+  static const String appIconPng = "assets/images/Icon (1).png";
+  static const String instaLogoPng = "assets/images/Instagram Logo.png";
+  static const String proPic = "assets/images/ajaspic.jpeg";
+  static const String facebookLogo = "assets/images/facebook_logo.png";
+}
---
+++ lib/view/splash_screen/splash_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_instagram_clone/core/constants/image_constants.dart';
+import 'package:flutter_instagram_clone/view/create_account_screen/create_accountscreen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    Future.delayed(Duration(seconds: 3)).then((value) {
+      Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => CreateAccountScreen(),
+          ));
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Image.asset(ImageConstants.appIconPng),
+      ),
+    );
+  }
+}
